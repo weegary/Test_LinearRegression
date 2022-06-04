a29366cdbbb959f99fb4228036b80d68d798a166
Gary <wgary111@hotmail.com>
2022-06-05 02:32:21 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MathNet.Numerics;
+
+namespace Test_LinearRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testA();
+            testB();
+            Console.ReadLine();
+        }
+
+        static void testA()
+        {
+            double[] xdata = new double[] { 10, 20 };
+            double[] ydata = new double[] { 0, 1 };
+
+            var p = (Fit.Line(xdata, ydata));
+            double a = p.Item1; 
+            double m = p.Item2; 
+            Console.WriteLine($"y = {m}x+{a}");
+        }
+    
+    static void testB()
+    {
+        double[] xdata = new double[] { 20, 30 };
+        double[] ydata = new double[] { 1, 0 };
+
+        var p = (Fit.Line(xdata, ydata));
+        double a = p.Item1; 
+        double m = p.Item2; 
+        Console.WriteLine($"y = {m}x+{a}");
+    }
+}
+}
